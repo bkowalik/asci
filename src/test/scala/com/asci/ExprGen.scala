9996a5e72f0e355c4f2ff3fd09eb7de988343f85
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-05-16 17:10:10 +0200

    Fixes in Expr generator

--- src/test/scala/com/asci/ExprGen.scala
+++ src/test/scala/com/asci/ExprGen.scala
@@ -6,11 +6,12 @@ import com.asci.Constant._
 import com.asci.Expr.{DottedList, ListExpr, Atom}
 
 object ExprGen {
+  // FIXME: add quotation generator
 
   def expr: Gen[Expr] = gen(0)
 
   // FIXME: pass depth limit as parameter
-  def gen(i: Int): Gen[Expr] = if (i > 3) leafs else lists(i + 1)
+  def gen(i: Int): Gen[Expr] = if (i > 2) leafs else lists(i + 1)
 
   def all(i: Int): Gen[Expr] = Gen.oneOf(leafs, lists(i))
 
@@ -18,35 +19,61 @@ object ExprGen {
 
   def lists(i: Int): Gen[Expr] = Gen.oneOf(properList(i), dottedList(i))
 
-  // FIXME: forbid generating invalid identifiers
-  def atom: Gen[Atom] = niceString.arbitrary flatMap Atom.apply
+  def atom: Gen[Atom] = GenHelpers.identifier flatMap Atom.apply
 
   def bool: Gen[BooleanConstant] = arbitrary[Boolean] flatMap BooleanConstant.apply
 
   lazy val niceChar: Arbitrary[Char] = Arbitrary(
-    Gen.choose(Char.MinValue, 127)
+    Gen.choose(32, 127)
   )
 
   def char: Gen[CharacterConstant] = niceChar.arbitrary flatMap CharacterConstant.apply
 
   lazy val niceString: Arbitrary[String] = Arbitrary(
-    Gen.listOf(niceChar.arbitrary) map (_.mkString)
+    Gen.listOfN(10, Gen.alphaNumChar) map (_.mkString)
   )
   def string: Gen[StringConstant] = niceString.arbitrary flatMap StringConstant.apply
 
-  def float: Gen[FloatingNum] = arbitrary[Float] flatMap FloatingNum.apply
+  def float: Gen[FloatingNum] = GenHelpers.simpleFloat flatMap FloatingNum.apply
 
-  def int: Gen[IntegerNum] = arbitrary[Int] flatMap IntegerNum.apply
+  def int: Gen[IntegerNum] = Gen.choose(0, Int.MaxValue) flatMap IntegerNum.apply
 
   // FIXME: do not ignore size parameter
   def properList(i: Int): Gen[ListExpr] = Gen.sized { size =>
-    Gen.listOfN(3, gen(i)) flatMap ListExpr.apply
+    Gen.listOfN(2, gen(i)) flatMap ListExpr.apply
   }
 
   def dottedList(i: Int): Gen[DottedList] = Gen.sized { size =>
     for {
-      n <- Gen.listOfN(3, gen(i))
+      n <- Gen.listOfN(2, gen(i))
       v <- gen(i)
     } yield DottedList(n, v)
+  }
+}
+
+object GenHelpers {
+
+  def identifier: Gen[String] = Gen.sized { size =>
+    for {
+      init <- Gen.oneOf(letter, initialSymbol)
+      rest <- Gen.listOfN(size % 10 + 1, Gen.oneOf(letter, digit, symbol))
+    } yield init + rest.mkString
+  }
+
+  def letter: Gen[Char] = Gen.alphaChar
+
+  def symbol: Gen[Char] = Gen.oneOf(initialSymbol, Gen.oneOf(".+-".toSeq))
+
+  def initialSymbol: Gen[Char] = Gen.oneOf("!$%&*/:<=>?~_^".toSeq)
+
+  def digit: Gen[Char] = Gen.numChar
+
+  def simpleFloat: Gen[Float] = for {
+    i <- Gen.nonEmptyListOf(digit)
+    f <- Gen.nonEmptyListOf(digit)
+  } yield {
+    val integral = i.take(6).mkString
+    val fractional = f.take(6).mkString
+    s"$integral.$fractional".toFloat
   }
 }
