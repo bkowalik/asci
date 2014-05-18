4e3b90ba49efe12851fd2073aed48f141c29ebed
Marcin Mikołajczyk <marcinmikolajcz@gmail.com>
2014-05-18 22:59:56 +0200

    EvalError Show instance

--- src/main/scala/com/asci/EvalError.scala
+++ src/main/scala/com/asci/EvalError.scala
@@ -1,5 +1,7 @@
 package com.asci
 
+import scalaz.Show
+
 sealed abstract class EvalError extends Throwable
 
 case class InvalidArgsNumber(valid: Int)               extends EvalError
@@ -9,3 +11,16 @@ case class TypeMismatch(expected: String, got: String) extends EvalError
 case class NotImplemented(name: String)                extends EvalError
 case class ParseError(err: String)                     extends EvalError
 case class OtherError(name: String)                    extends EvalError
+
+object EvalError {
+  implicit object ShowEvalError extends Show[EvalError] {
+    override def shows(e: EvalError): String = e match {
+      case InvalidArgsNumber(valid) => s"Invalid number of arguments passed, required $valid"
+      case UnboundVariable(a)       => s"Unbound variable $a"
+      case TypeMismatch(ex, g)      => s"Type mismatch, expected $ex, got $g"
+      case NotImplemented(f)        => s"Feature $f is not yet implemented"
+      case ParseError(err)          => s"Parse error: $err"
+      case OtherError(err)          => err
+    }
+  }
+}
